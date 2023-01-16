b3ab070c94d7d6bc7cb80c8e384a35f4be4161d2
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-01-16 15:54:57 +0300

    3. Создание вакансии [#504849 #373697]

---
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -0,0 +1,34 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.dreamjob.model.Vacancy;
+import ru.job4j.dreamjob.repository.MemoryVacancyRepository;
+import ru.job4j.dreamjob.repository.VacancyRepository;
+
+@Controller
+@RequestMapping("/vacancies")
+public class VacancyController {
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("vacancies", vacancyRepository.findAll());
+        return "vacancies/list";
+    }
+
+    @GetMapping("/create")
+    public String getCreationPage() {
+        return "vacancies/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Vacancy vacancy) {
+        vacancyRepository.save(vacancy);
+        return "redirect:/vacancies";
+    }
+}
