880bb2aa85a314e1d165f56025db687e6f8edaaf
Natalia Khmeleva <nataliya-mar@mail.ru>
2023-01-16 15:43:42 +0300

    3. Создание вакансии [#504849 #373697]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -7,7 +7,7 @@ public class Vacancy {
     private int id;
     private String title;
     private String description;
-    private LocalDateTime creationDate;
+    private LocalDateTime creationDate = LocalDateTime.now();
 
     public Vacancy(int id, String title, String description, LocalDateTime creationDate) {
         this.id = id;
